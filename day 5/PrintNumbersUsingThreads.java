ef13d8228ca65378cc23cd00e6835ba8921c5736
varshini41 <111384789+varshini41@users.noreply.github.com>
2023-12-30 11:21:09 +0530

    Add files via upload

---
+++ day 5/PrintNumbersUsingThreads.java
@@ -0,0 +1,40 @@
+
+class PrintNumbers implements Runnable {
+    private static final Object lock = new Object();
+    private static int currentNumber = 1;
+    private final int maxNumber;
+
+    public PrintNumbers(int maxNumber) {
+        this.maxNumber = maxNumber;
+    }
+
+    @Override
+    public void run() {
+        while (currentNumber <= maxNumber) {
+            synchronized (lock) {
+                // Check if it's the thread's turn to print
+                if (currentNumber <= maxNumber) {
+                    // Print the current number and increment it
+                    System.out.println(Thread.currentThread().getName() + ": " + currentNumber++);
+                }
+            }
+        }
+    }
+}
+
+public class PrintNumbersUsingThreads {
+    public static void main(String[] args) {
+        int N = 18;
+
+        // Create two threads and pass the same instance of PrintNumbers
+        PrintNumbers printNumbers = new PrintNumbers(N);
+        Thread t1 = new Thread(printNumbers, "Thread-1");
+        Thread t2 = new Thread(printNumbers, "Thread-2");
+
+        // Start the threads
+        t1.start();
+        t2.start();
+    }
+}
+
+
